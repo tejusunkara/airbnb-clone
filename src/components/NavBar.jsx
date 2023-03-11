5028500156d4a53be3ec22b2c7cddb652526f2ee
Venkata Sunkara <jnanatejaswini@gmail.com>
2023-03-11 17:22:25 -0500

    nav bar component added

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row shadow-md">
+      <nav className="py-5 px-9 h-18">
+        <img
+          src="/assets/airbnb-logo.png"
+        />
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
